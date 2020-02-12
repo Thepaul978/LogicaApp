e3947fefad9263a28332f47b3a75cd5d3216f955
Thepaul978 <pauloomen1997@gmail.com>
2020-02-13 00:11:55 +0100

    Initial commit

---
+++ app/src/main/java/com/example/logicaapp/LogicaActivity.kt
@@ -0,0 +1,40 @@
+package com.example.logicaapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_logica.*
+
+class LogicaActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_logica)
+        initViews()
+    }
+
+    private fun initViews() {
+        button.setOnClickListener {
+            checkInput()
+        }
+    }
+
+    private fun checkInput() {
+        var count: Int = 0;
+        if (editText.text.toString() == "T") {
+            count++;
+        }
+        if (editText2.text.toString() == "F") {
+            count++;
+        }
+        if (editText3.text.toString() == "F") {
+            count++;
+        }
+        if (editText4.text.toString() == "F") {
+            count++;
+        }
+        Toast.makeText(this, "The Correct number of answers is: " + count, Toast.LENGTH_SHORT).show()
+    }
+}
